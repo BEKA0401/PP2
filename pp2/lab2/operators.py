3560bcbd213971829854c93e672a3f6911e78061
Кумисбеков Бексултан <kumisbekovbeksultan@MacBook-Air-Kumisbekov.local>
2024-01-31 21:26:35 +0600

    lab2

---
+++ pp2/lab2/operators.py
@@ -0,0 +1,29 @@
+#1Exercise
+print(10*5)
+#2Exercise
+print(10/2)
+#3Exercise
+fruits = ["apple", "banana"]
+if "apple"  in fruits:
+  print("Yes, apple is a fruit!")
+#4Exercise
+if 5!=10:
+  print("5 and 10 is not equal")
+  #5Exercise
+if 5 == 10 or 4 == 4:
+  print("At least one of the statements is true")
+  #6Exercise 
+  fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+#7Exercise 
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi"
+print(fruits)
+#8Exercise 
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+print(fruits)
+#9Exercise
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1,"lemon")
+print(fruits)
---
+++ pp2/lab2/tuples.py
@@ -0,0 +1,12 @@
+#1Exercise
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
+#2Exercise
+fruits = ("apple", "banana", "cherry")
+print(len(fruits))
+#3Exercise
+fruits = ("apple", "banana", "cherry")
+print(fruits[-1])
+#4Exercise
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])
